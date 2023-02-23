2c0a52a8a728f86c84465cacc9424cb41d7d5638
codeacme17 <vir_kings@qq.com>
2023-02-23 11:42:55 +0800

    add: create react project

--- package/command/create.js
+++ package/command/create.js
@@ -1,12 +1,11 @@
 const fs = require("fs")
 const path = require("path")
 const chalk = require("chalk")
-const boxen = require("boxen")
 
 const create = require("../handler/create-preject")
 const creativeQuestions = require("../question/create")
 const { COLORS } = require("../utils/config")
-const { log, clear, error, success } = require("../utils/log")
+const { clear, error } = require("../utils/log")
 const { TARGET } = require("../utils/path")
 
 module.exports = async (name) => {
@@ -26,8 +25,6 @@ module.exports = async (name) => {
   const framework = await setFramework()
 
   create[framework](TARGET_DIR, name)
-
-  successLog(name)
 }
 
 async function setPrjectName() {
@@ -38,12 +35,4 @@ async function setPrjectName() {
 async function setFramework() {
   const answer = await creativeQuestions.framework()
   return answer.FRAMEWORK
-}
-
-function successLog(name) {
-  success(`Successfully created ${chalk.hex(COLORS.GREEN)(name)}`)
-  log(`   ${chalk.hex(COLORS.YELLOW)("cd")} ${name}`)
-  log()
-  log(`   ${chalk.hex(COLORS.YELLOW)("pnpm")} install`)
-  log()
 }
--- package/handler/create-preject.js
+++ package/handler/create-preject.js
@@ -1,24 +1,45 @@
 const shell = require("shelljs")
-const path = require("path")
 const chalk = require("chalk")
 
 const { SOURCE_PROJECT } = require("../utils/path")
-const { log } = require("../utils/log")
+const { log, success } = require("../utils/log")
+const { COLORS } = require("../utils/config")
 
 shell.config.fatal = true
 
-function vue(TARGET_DIR, name) {
-  startCreateLog(name)
+function vue(TARGET_DIR, project) {
+  startCreateLog(project)
+
   shell.mkdir("-p", TARGET_DIR)
   shell.cp("-R", SOURCE_PROJECT.vue, TARGET_DIR)
+
+  successLog(project)
+}
+
+function react(TARGET_DIR, project) {
+  startCreateLog(project)
+
+  shell.mkdir("-p", TARGET_DIR)
+  shell.cp("-R", SOURCE_PROJECT.react, TARGET_DIR)
+
+  successLog(project)
 }
 
 module.exports = {
   vue,
+  react,
 }
 
 function startCreateLog(prject) {
   log()
   log(`📦  Creating ${chalk.cyan(prject)}...`)
+  log()
+}
+
+function successLog(name) {
+  success(`Successfully created ${chalk.hex(COLORS.GREEN)(name)}`)
+  log(`   ${chalk.hex(COLORS.YELLOW)("cd")} ${name}`)
+  log()
+  log(`   ${chalk.hex(COLORS.YELLOW)("pnpm")} install`)
   log()
 }
